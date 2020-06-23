5901d3d0f90b07728bd4846343107844891a3c56
Ruben <malakasrbn@gmail.com>
2020-06-23 15:11:55 +0200

    forms template added

--- Ruben_Day02/CarRental/src/app/app.module.ts
+++ Ruben_Day02/CarRental/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Ruben_Day02/CarRental/src/app/contact/contact.component.ts
+++ Ruben_Day02/CarRental/src/app/contact/contact.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
+  selector: 'contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
 
+  info = new FormGroup({
+    firstName: new FormControl('',Validators.required),
+    lastName: new FormControl('',Validators.required),
+    subject: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    address: new FormGroup({
+      street: new FormControl('',Validators.required),
+      city: new FormControl('',Validators.required),
+      state: new FormControl('',Validators.required),
+      zip: new FormControl('',Validators.required)
+    })
+
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+
+    if(this.info.valid){
+      var a = this.info.value;
+      console.log(a)
+   }
+
   }
 
 }
